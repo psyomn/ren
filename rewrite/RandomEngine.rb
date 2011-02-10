d5918fa53bb95ea0f34d4be713e564e18f27cfaa
psyomn <psyomn@xenagoras.net>
2011-02-10 17:30:38 -0500

    Saving some new stuff.

--- rewrite/RandomEngine.rb
+++ rewrite/RandomEngine.rb
@@ -11,6 +11,7 @@ class RandomEngine
 	attr_accessor :mCommand
 	attr_accessor :mDelim1
 	attr_accessor :mDelim2
+	attr_accessor :mIterations
 
 	attr_reader :mMaleNameLocation
 	attr_reader :mFemaleNameLocation
@@ -39,82 +40,11 @@ class RandomEngine
 
 		@mResult = ""
 		@mCommand = ""
+
+		@mIterations = 1
 	end
 	
-	# Definition to execute the command. 
-	def execute(val)
-		@mResult = ""
-		commandParser(val)
-		
-		addressArr = ["Ave.","St.","Way","Cir","Dr","Rd","Ct","Trl","Blvd"]
-		
-		@mCommand.split.each{ |option|
-			case option
-				when "id"
-				when "rid"
-				when "surname"
-				when "address"
-					@mResult += words[rand(words.size)].capitalize + " " + words[rand(words.size)].capitalize + " " + addressArr.sample
-				when "name"
-				when "post"
-					@mResult += (65 + rand(26)).chr + rand(9).to_s + (65 + rand(26)).chr + rand(9).to_s + (65 + rand(26)).chr + rand(9).to_s
-				when "age"
-					@mResult += (rand(40) + 14).to_s
-				when "phone"
-					@mResult += (100+rand(899)).to_s + "-" + ("%04d" % rand(9999))
-				when "alpha"
-					@mResult += (97 + rand(26)).chr
-				when "ALPHA"
-					@mResult += (65 + rand(26)).chr
-				when "num"
-					@mResult += (rand(10)).to_s # 0 -> 9
-				when "date"
-					month = rand(12) + 1
-					t = Time.utc(1970+rand(500), month, 1+rand(31), 1+rand(23),rand(60),rand(60)) 
-					@mResult += t.year.to_s + "-" + "%02d" % t.mon + "-" + "%02d" % t.day
-				when "time"
-					t = Time.utc(1970+rand(500), month, 1+rand(31), 1+rand(23),rand(60),rand(60)) 
-					@mResult += t.hour.to_s + ":" +  t.min.to_s + ":" +  t.sec.to_s
-				when "space" # TODO
-					@mResult += "  " 
-					nodelim = true
-				when "null"  # TODO
-					#@mResult += $delim2 
-				when /word/
-					w = ARGV[x]
-					n = w.to_i
-					(n == 0 ? 1 : n).times{ @mResult += words[rand(words.length)] }
-				when /range/ 
-					expression = rangeHash[ARGV[x]] 
-					exp = expression.split('X')
-					@mResult += (rand(exp[1].to_i + 1) + exp[0].to_i).to_s
-				when /list/
-					l = ARGV[x]
-					l = l.gsub(/list/, '')
-					l = l.gsub(/=/, '')
-					la= l.split(',')
-					@mResult += la.sample
-				when /man/
-					l = ARGV[x]
-					l = l.gsub(/man/, '')
-					l = l.gsub(/=/, '')
-					@mResult += l
-				when "back"
-					2.times { @mResult.chop! }
-				when "nodelim"
-					nodelim = true
-			end # end the big case!
-		}
-	end
-
-	# Make sure to clear the results
-	def clearResult
-		@mResult.clear
-	end
-
-private
-
-	# This definition generates the needed information# Preprosses the command to see if special tokens such as 'times' exists
+	# This definition generates the needed information
 	#  o Some commands that might be worth implementing: 
  	#    o dateo      : oracle date input 
  	#    o Space      : a command to create a blank space
@@ -148,9 +78,93 @@ private
  	#    o nodelim    : Special rule for when you want to stick two things together
  	#                   for example year generation could be of the form
  	# for special cases as times
+	def execute(val)
+		@mResult = ""
+		commandParser(val)
+		
+		addressArr = ["Ave.","St.","Way","Cir","Dr","Rd","Ct","Trl","Blvd"]
+		@mIterations.times {		
+			@mCommand.split.each{ |option|
+				case option
+					when "id" # TODO
+					when "rid" # TODO 
+					when "surname" # TODO
+					when "address" 
+						@mResult += words[rand(words.size)].capitalize + " " + words[rand(words.size)].capitalize + " " + addressArr.sample
+					when "name" #TODO
+					when "post"
+						@mResult += (65 + rand(26)).chr + rand(9).to_s + (65 + rand(26)).chr + rand(9).to_s + (65 + rand(26)).chr + rand(9).to_s
+					when "age"
+						@mResult += (rand(40) + 14).to_s
+					when "phone"
+						@mResult += (100+rand(899)).to_s + "-" + ("%04d" % rand(9999))
+					when "alpha"
+						@mResult += (97 + rand(26)).chr
+					when "ALPHA"
+						@mResult += (65 + rand(26)).chr
+					when "num"
+						@mResult += (rand(10)).to_s # 0 -> 9
+					when "date"
+						month = rand(12) + 1
+						t = Time.utc(1970+rand(500), month, 1+rand(31), 1+rand(23),rand(60),rand(60)) 
+						@mResult += t.year.to_s + "-" + "%02d" % t.mon + "-" + "%02d" % t.day
+					when "time"
+						t = Time.utc(1970+rand(500), month, 1+rand(31), 1+rand(23),rand(60),rand(60)) 
+						@mResult += t.hour.to_s + ":" +  t.min.to_s + ":" +  t.sec.to_s
+					when "space" # TODO
+						@mResult += "  " 
+						nodelim = true
+					when "null"  # TODO
+						#@mResult += $delim2 
+					when /word/
+						# (n == 0 ? 1 : n).times{ @mResult += words[rand(words.length)] }
+					when /range/ 
+						# expression = rangeHash[ARGV[x]] 
+						# exp = expression.split('X')
+						# @mResult += (rand(exp[1].to_i + 1) + exp[0].to_i).to_s
+					when /list/
+						l = option 
+						l = l.gsub(/list/, '')
+						l = l.gsub(/=/, '')
+						la= l.split(',')
+						@mResult += la.sample
+					when /man/
+						l = option
+						l = l.gsub(/man/, '')
+						l = l.gsub(/=/, '')
+						@mResult += l
+					when "back"
+						2.times { @mResult.chop! }
+					when "nodelim"
+						nodelim = true
+				end # end the big case!
+	
+				@mResult += @mDelim1
+			}
+			@mResult += @mDelim2
+		}
+	end
+
+	# Make sure to clear the results
+	def clearResult
+		@mResult.clear
+	end
+
+private
+
+	# Preprosses the command to see if special tokens such as 'times' exists
 	def commandParser(cm)
 		@mCommand = ""
 		cm = cm.split	
+	
+		raise "Command has no options " if cm.size == 0
+		cm[0] = cm[0].to_i
+		raise "First option must be positive num" if cm[0] < 1
+
+		@mIterations = cm[0].to_i
+
+		cm.delete_at(0)
+
 		cm.each_index{ |token_index|
 			if cm[token_index] =~ /times/
 				if cm.size > token_index+1
@@ -159,7 +173,7 @@ private
 					}
 				end
 			else
-				@mCommand += cm[token_index] + " "
+				@mCommand += cm[token_index].to_s + " "
 			end
 		}
 	end
